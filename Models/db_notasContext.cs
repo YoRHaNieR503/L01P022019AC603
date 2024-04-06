36d0efaf07330c7f3c0131b3bd07ceceb150e91d
YoRHaNieR503 <christpoher.alarcon@catolica.edu.sv>
2024-04-06 09:00:31 -0600

    Agregar archivos de proyecto.

---
+++ Models/db_notasContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01P022019AC603.Models
+{
+    public class db_notasContext:DbContext
+    {
+
+        public db_notasContext(DbContextOptions<db_notasContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<>
+
+    }
+}
